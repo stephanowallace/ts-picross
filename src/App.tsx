28e2d92dcf9c0fdd5dc9c8ca32fa344ec6d70c85
Stephano Wallace Saraiva e Silva <stephano.silva@HOTBHZA003094.local>
2020-07-30 21:27:11 -0300

    add grid to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 import "./App.css";
 import CellTypeSelector from "./components/CellTypeSelector";
 import { CellContext } from "./contexts/CellType.context";
 import useCell from "./hooks/useCell";
-import Cell from "./components/Cell";
+import Grid from "./components/Grid";
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Content = styled.div`
+  align-items: center;
+  display: flex;
+`;
 
 function App() {
   const cell = useCell();
 
   return (
     <CellContext.Provider value={cell}>
-      <div className="App">
-        <CellTypeSelector />
-        <Cell />
-      </div>
+      <Container>
+        <Content>
+          <Grid rows={10} columns={15} />
+          <CellTypeSelector />
+        </Content>
+      </Container>
     </CellContext.Provider>
   );
 }
