68a2cfbebb9e09c2b4618da51ebcf85027475ce3
Stephano Wallace Saraiva e Silva <stephano.silva@HOTBHZA003094.local>
2020-07-30 21:26:21 -0300

    add style to cell component

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,11 +1,50 @@
 import React, { useContext, useState } from "react";
+import styled from "styled-components";
 import { CellContext } from "../../contexts/CellType.context";
+
+interface IContainerProps {
+  state: CellType;
+}
+
+const Container = styled.div<IContainerProps>`
+  align-items: center;
+  background-color: ${({ state }) => (state === "Fill" ? "blue" : "white")};
+  border: 2px solid lightgray;
+  border-radius: 4px;
+  color: ${({ state }) => (state === "Ignore" ? "red" : "blue")};
+  cursor: pointer;
+  display: flex;
+  font-weight: bold;
+  height: 24px;
+  justify-content: center;
+  transition: all 0.25s;
+  width: 24px;
+
+  &:hover {
+    border: 2px solid black;
+  }
+`;
 
 const Cell = () => {
   const { type } = useContext(CellContext);
   const [cellState, setCellState] = useState<CellType>("Void");
 
-  return <button onClick={() => setCellState(type)}>{cellState}</button>;
+  const renderContent = () => {
+    switch (cellState) {
+      case "Ignore":
+        return "X";
+      case "Doubt":
+        return "?";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <Container onClick={() => setCellState(type)} state={cellState}>
+      {renderContent()}
+    </Container>
+  );
 };
 
 export default Cell;
