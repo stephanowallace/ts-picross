eea6d336fe1d881b2e06cfc089494137008aff2c
Stephano Wallace Saraiva e Silva <stephano.silva@HOTBHZA003094.local>
2020-07-30 21:27:02 -0300

    container with buttons to improve usability

--- src/components/CellTypeSelector/index.tsx
+++ src/components/CellTypeSelector/index.tsx
@@ -1,15 +1,27 @@
 import React, { useContext } from "react";
+import styled from "styled-components";
 import { CellContext } from "../../contexts/CellType.context";
+
+const Container = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  margin-left: 24px;
+`;
+
+const Button = styled.button`
+  margin-bottom: 10px;
+`;
 
 const CellTypeSelector: React.FC = () => {
   const { changeType } = useContext(CellContext);
 
   return (
-    <div>
-      <button onClick={() => changeType("Ignore")}>x</button>
-      <button onClick={() => changeType("Fill")}>v</button>
-      <button onClick={() => changeType("Doubt")}>?</button>
-    </div>
+    <Container>
+      <Button onClick={() => changeType("Ignore")}>x</Button>
+      <Button onClick={() => changeType("Fill")}>v</Button>
+      <Button onClick={() => changeType("Doubt")}>?</Button>
+      <Button onClick={() => changeType("Void")}>-</Button>
+    </Container>
   );
 };
 
