1ff4217f88f6c01d0b323f77329f5a47186cbc1c
Stephano Wallace Saraiva e Silva <stephano.silva@HOTBHZA003094.local>
2020-08-03 21:40:00 -0300

    start handling cell changes

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Cell from "../Cell";
 import styled from "styled-components";
+import { DrawMatrixContext } from "../../contexts/DrawMatrix.context";
+import { countFilledCells } from "../../utils/matrix";
 
 const Container = styled.div`
   display: flex;
@@ -12,16 +14,33 @@ const Row = styled.div`
 `;
 
 interface IGridProps {
-  columns?: Number;
-  rows?: Number;
+  columns?: number;
+  rows?: number;
 }
 
 const Grid: React.FC<IGridProps> = ({ columns = 5, rows = 5 }) => {
+  const { matrix, createMatrix, setMatrix } = useContext(DrawMatrixContext);
+
+  useEffect(() => {
+    createMatrix(rows, columns);
+  }, []);
+
+  const handleCellChange = (x: number, y: number, cellType: CellType) => {
+    matrix[x][y] = cellType;
+    setMatrix(matrix);
+    console.log(">>>>", matrix);
+  };
+
+  countFilledCells(["Void", "Fill", "Fill", "Void", "Fill", "Void"]);
+
   const renderCells = () => {
-    return [...Array(rows)].map((_, index) => (
-      <Row key={`row-${index}`}>
-        {[...Array(columns)].map((_, index) => (
-          <Cell key={`col-${index}`} />
+    return [...Array(rows)].map((_, x) => (
+      <Row key={`row-${x}`}>
+        {[...Array(columns)].map((_, y) => (
+          <Cell
+            key={`col-${y}`}
+            onChange={(cellType: CellType) => handleCellChange(x, y, cellType)}
+          />
         ))}
       </Row>
     ));
